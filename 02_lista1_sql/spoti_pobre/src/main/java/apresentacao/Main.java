320eb879218df2fce7271567fd2c7ec44b9b3f64
Igor Avila Pereira <igor.pereira@riogrande.ifrs.edu.br>
2026-08-18 20:39:58 -0300

    atualizando

--- 02_lista1_sql/spoti_pobre/src/main/java/apresentacao/Main.java
+++ 02_lista1_sql/spoti_pobre/src/main/java/apresentacao/Main.java
@@ -2,18 +2,38 @@ package apresentacao;
 
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
 
+import io.javalin.Javalin;
 import negocio.Usuario;
 import persistencia.UsuarioDAO;
 
 public class Main {
     public static void main(String[] args) throws SQLException {
-        Usuario igor = new Usuario();
-        igor.setDataNascimento(LocalDate.of(1987, 01, 20));
-        igor.setNome("Igor Pereira");
-        igor.setEmail("igor.pereira@riogrande.ifrs.edu.br");
-        igor.setSenha("123");
+        // Usuario novo_usuario = new Usuario();
+        // novo_usuario.setDataNascimento(LocalDate.of(1987, 01, 20));
+        // novo_usuario.setNome("Joao");
+        // novo_usuario.setEmail("joao@riogrande.ifrs.edu.br");
+        // novo_usuario.setSenha("123");
+        // new UsuarioDAO().salvar(novo_usuario);
+        // new UsuarioDAO().deletar(22);
+        // Usuario vanessa = new UsuarioDAO().obter(21);
+        // vanessa.setNome("Vanessa Pereira");
+        // new UsuarioDAO().atualizar(vanessa);
+        // new UsuarioDAO().listar().forEach(u ->
+        // System.out.println(u.getId()+";"+u.getNome()));
 
-        new UsuarioDAO().salvar(igor);
+        Javalin app = Javalin.create(config -> {
+            config.routes.get("/", ctx -> {
+                ArrayList<Usuario> vet = new UsuarioDAO().listar();
+                String html = "<h1> Site do Igor </h1> <ul>";
+                for (Usuario usuario : vet) {
+                    html += "<li>" + usuario.getNome()+"</li>";
+                }
+                html += "</ul>";        
+                ctx.html(html);
+            });
+        }).start(7070);
+
     }
 }
--- 02_lista1_sql/spoti_pobre/src/main/java/persistencia/UsuarioDAO.java
+++ 02_lista1_sql/spoti_pobre/src/main/java/persistencia/UsuarioDAO.java
@@ -3,13 +3,51 @@ package persistencia;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.time.LocalDate;
+import java.util.ArrayList;
 
 import negocio.Usuario;
 
 // DAO: Data Acess Object
 public class UsuarioDAO {
+
+    public ArrayList<Usuario> listar() throws SQLException {
+        ArrayList<Usuario> vetUsuario = new ArrayList<Usuario>();
+        String sql = "SELECT * FROM usuario ORDER BY id;";
+        Connection conexao = new ConexaoPostgreSQL().getConexao();
+        PreparedStatement instrucaoSQL = conexao.prepareStatement(sql);
+        ResultSet rs = instrucaoSQL.executeQuery();
+        while (rs.next()) {
+            Usuario usuario = new Usuario();
+            usuario.setId(rs.getInt("id"));
+            usuario.setDataNascimento(rs.getDate("data_nascimento").toLocalDate());
+            usuario.setEmail(rs.getString("email"));
+            usuario.setNome(rs.getString("nome"));
+            // usuario.setSenha(rs.getString("senha"));
+            vetUsuario.add(usuario);
+        }
+        conexao.close();
+        return vetUsuario;
+    }
+
+    public Usuario obter(int id) throws SQLException {
+        Usuario usuario = new Usuario();
+        String sql = "SELECT * FROM usuario where id = ?;";
+        Connection conexao = new ConexaoPostgreSQL().getConexao();
+        PreparedStatement instrucaoSQL = conexao.prepareStatement(sql);
+        instrucaoSQL.setInt(1, id);
+        ResultSet rs = instrucaoSQL.executeQuery();
+        if (rs.next()) {
+            usuario.setId(rs.getInt("id"));
+            usuario.setDataNascimento(rs.getDate("data_nascimento").toLocalDate());
+            usuario.setEmail(rs.getString("email"));
+            usuario.setNome(rs.getString("nome"));
+            usuario.setSenha(rs.getString("senha"));
+        }
+        conexao.close();
+        return usuario;
+    }
 
     public void salvar(Usuario usuario) throws SQLException {
         String sql = "INSERT INTO usuario (email, senha, nome, data_nascimento) VALUES (?,md5(?),?,?);";
@@ -21,7 +59,30 @@ public class UsuarioDAO {
         instrucaoSQL.setDate(4, Date.valueOf(usuario.getDataNascimento()));
         instrucaoSQL.execute();
         conexao.close();
-        
+
+    }
+
+    public void deletar(int id) throws SQLException {
+        String sql = "DELETE FROM usuario WHERE id = ?;";
+        Connection conexao = new ConexaoPostgreSQL().getConexao();
+        PreparedStatement instrucaoSQL = conexao.prepareStatement(sql);
+        instrucaoSQL.setInt(1, id);
+        instrucaoSQL.execute();
+        conexao.close();
+    }
+
+    public void atualizar(Usuario usuario) throws SQLException {
+        String sql = "UPDATE usuario SET email = ?, senha = ?, nome = ?, data_nascimento = ? where id = ?;";
+        Connection conexao = new ConexaoPostgreSQL().getConexao();
+        PreparedStatement instrucaoSQL = conexao.prepareStatement(sql);
+        instrucaoSQL.setString(1, usuario.getEmail());
+        instrucaoSQL.setString(2, usuario.getSenha());
+        instrucaoSQL.setString(3, usuario.getNome());
+        instrucaoSQL.setDate(4, Date.valueOf(usuario.getDataNascimento()));
+        instrucaoSQL.setInt(5, usuario.getId());
+        instrucaoSQL.execute();
+        conexao.close();
+
     }
 
 }
